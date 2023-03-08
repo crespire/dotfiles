ed82050dde2f0e195e81d95a2f1983f2096552a4
crespire <hello@crespire.dev>
2023-03-08 18:24:24 -0500

    fix: use installer for rbenv

--- install/rbenv.sh
+++ install/rbenv.sh
@@ -1,4 +1,3 @@
 # Installs rbenv
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-. ~/.bashrc
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
 rbenv install 3.1.2 && rbenv global 3.1.2
