84ea220c49783a81e9136776d46772d42746f853
crespire <hello@crespire.dev>
2023-03-09 14:04:40 -0500

    fix: update utils

--- install/utils.sh
+++ install/utils.sh
@@ -1,4 +1,6 @@
 sudo apt-get -y update && sudo apt-get -y upgrade
 sudo apt-get -y install software-properties-common
 sudo apt-get -y install build-essential
+sudo apt-get -y install libssl-dev
+sudo apt-get -y install zlib1g zlib1g-dev
 sudo apt-get -y install ripgrep
