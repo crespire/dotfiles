f05dc41f6716cc402a643b1ce82efa9033e1946e
crespire <hello@crespire.dev>
2023-03-08 21:01:22 -0500

    fix: update installs to install only

--- install/nvm.sh
+++ install/nvm.sh
@@ -1,3 +1,1 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
-source ~/.bashrc
-nvm install --lts && nvm use --lts
--- install/rbenv.sh
+++ install/rbenv.sh
@@ -1,3 +1,2 @@
 # Installs rbenv
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
-rbenv install 3.1.2 && rbenv global 3.1.2
