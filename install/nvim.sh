ccaf7514018d22404a013bfc2a3733fdd6c7ef7b
crespire <hello@crespire.dev>
2023-03-09 10:35:44 -0500

    fix: clone packer to packpath

--- install/nvim.sh
+++ install/nvim.sh
@@ -3,3 +3,5 @@ sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt -y update
 sudo apt-get -y install software-properties-common
 sudo apt -y install neovim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
