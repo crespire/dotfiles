73c49b12f70762fce52473df64fb90c460873d90
crespire <hello@crespire.dev>
2023-03-09 11:21:53 -0500

    fix: see if nvim will work if I just don't do the other part

--- install/nvim.sh
+++ install/nvim.sh
@@ -3,6 +3,3 @@ sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt -y update
 sudo apt-get -y install software-properties-common
 sudo apt -y install neovim
-
-echo "Adding plugins via Packer..."
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
