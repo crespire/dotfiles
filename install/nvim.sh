b2fa560d85ba31d432b6af477db8332dae8991c6
crespire <hello@crespire.dev>
2023-03-09 11:42:16 -0500

    fix: move some utils around to different install spots

--- install/nvim.sh
+++ install/nvim.sh
@@ -1,7 +1,5 @@
 # Add PPA for Unstable (nightly)
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
-sudo apt -y update
-sudo apt-get -y install software-properties-common
 sudo apt -y install neovim
 
 . "$HOME/.bashrc"
--- install/utils.sh
+++ install/utils.sh
@@ -1,3 +1,4 @@
+sudo apt-get -y update && apt-get -y upgrade
+sudo apt-get -y instgall software-properties-common
 sudo apt-get -y install build-essential
 sudo apt-get -y install ripgrep
-
