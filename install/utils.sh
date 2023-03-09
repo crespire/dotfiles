8e0c883fcc9eaa54a0130815d51cedf58a95518a
crespire <hello@crespire.dev>
2023-03-09 11:45:18 -0500

    fix: typo

--- install/utils.sh
+++ install/utils.sh
@@ -1,4 +1,4 @@
 sudo apt-get -y update && sudo apt-get -y upgrade
-sudo apt-get -y instgall software-properties-common
+sudo apt-get -y install software-properties-common
 sudo apt-get -y install build-essential
 sudo apt-get -y install ripgrep
