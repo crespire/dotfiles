9e16e0eeaa0da20ac250038de5e8172eb37c3205
crespire <hello@crespire.dev>
2023-03-27 13:58:26 -0400

    fix: install nerdfont, comment out Go

--- install/utils.sh
+++ install/utils.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Utilities
 sudo apt-get -y update && sudo apt-get -y upgrade
 sudo apt-get -y install software-properties-common
 sudo apt-get -y install build-essential
@@ -7,5 +8,12 @@ sudo apt-get -y install libssl-dev
 sudo apt-get -y install zlib1g zlib1g-dev
 sudo apt-get -y install ripgrep
 
+# Install Jetbrains Mono Nerdfonts
+echo "Downloading JetBrains Mono..."
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/JetBrainsMono.zip
+unzip JetBrainsMono.zip -d ~/.fonts
+fc-cache -fv
+echo "Installed JetBrains Mono!"
+
 # Go Lint
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.52.2
+# sudo curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.52.2
