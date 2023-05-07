be44e80a94ff07aa829311b0f2ae0e86c241592e
crespire <hello@crespire.dev>
2023-05-07 12:12:27 -0400

    fix typo

--- install/utils.sh
+++ install/utils.sh
@@ -14,7 +14,7 @@ echo "Installing Lazygit"
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
-sudo install lazyfit /usr/local/bin
+sudo install lazygit /usr/local/bin
 rm lazygit
 rm lazygit.tar.gz
 echo "Installed Lazygit!"
