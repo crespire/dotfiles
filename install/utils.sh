06dbb2538f2cefdce10fd1ce45a7e9e9da31f889
crespire <hello@crespire.dev>
2023-05-07 11:55:38 -0400

    Update utils, set options for neotree

--- install/utils.sh
+++ install/utils.sh
@@ -7,6 +7,17 @@ sudo apt-get -y install build-essential
 sudo apt-get -y install libssl-dev
 sudo apt-get -y install zlib1g zlib1g-dev
 sudo apt-get -y install ripgrep
+sudo apt-get -y install fd-find
+
+# Install Lazygit
+echo "Installing Lazygit"
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazyfit /usr/local/bin
+rm lazygit
+rm lazygit.tar.gz
+echo "Installed Lazygit!"
 
 # Install Jetbrains Mono Nerdfonts
 echo "Downloading JetBrains Mono..."
