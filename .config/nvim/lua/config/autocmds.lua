bb43350ee7cb21be0a9c720e4b83d0aa6cd2103b
crespire <hello@crespire.dev>
2023-05-10 15:57:24 -0400

    Open all folds by default

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,6 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Open all folds on Buffer Load
+vim.api.nvim_create_autocmd({ "BufRead", "BufWinEnter", "BufReadPost", "FileReadPost" }, { command = "normal zR" })
