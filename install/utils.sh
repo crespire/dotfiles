fe0a5974bf39cd76a4552da70a6569eacbd31763
crespire <hello@crespire.dev>
2023-06-04 19:21:40 -0400

    Add libyaml-dev

--- install/utils.sh
+++ install/utils.sh
@@ -5,6 +5,7 @@ sudo apt-get -y update && sudo apt-get -y upgrade
 sudo apt-get -y install software-properties-common
 sudo apt-get -y install build-essential
 sudo apt-get -y install libssl-dev
+sudo apt=get -y install libyaml-dev
 sudo apt-get -y install zlib1g zlib1g-dev
 sudo apt-get -y install ripgrep
 sudo apt-get -y install fd-find
